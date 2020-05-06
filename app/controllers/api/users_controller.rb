d1715b698ee207ac406fb9e83187ba18b12d90d4
gv4383 <gregory.vargas18@gmail.com>
2020-05-06 00:29:19 -0500

    Created users#create method

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,5 +1,7 @@
 module Api
   class UsersController < ApplicationController
+    protect_from_forgery with: :null_session
+
     def index
       users = User.all
 
@@ -10,6 +12,22 @@ module Api
       user = User.find(params[:id])
 
       render json: UserSerializer.new(user).serialized_json
+    end
+
+    def create
+      user = User.new(user_params)
+
+      if user.save
+        render json: UserSerializer.new(user).serialized_json
+      else
+        render json: { error: product.errors.messages }, status: 400
+      end
+    end
+
+    private
+
+    def user_params
+      params.require(:user).permit(:first_name, :last_name, :email)
     end
   end
 end
