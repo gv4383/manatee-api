e12efbeffb352298f13fcef442e05e52041da18a
gv4383 <gregory.vargas18@gmail.com>
2020-05-05 23:35:47 -0500

    Rollback users table and add details
    Added columns for first and last names

--- db/schema.rb
+++ db/schema.rb
@@ -16,7 +16,8 @@ ActiveRecord::Schema.define(version: 2020_05_06_025402) do
   enable_extension "plpgsql"
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
+    t.string "first_name"
+    t.string "last_name"
     t.string "email"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
