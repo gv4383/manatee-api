a5699476a8e640efd2964eb942a253d203802748
gv4383 <gregory.vargas18@gmail.com>
2020-05-05 23:37:54 -0500

    Add user seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+users = User.create([
+  {
+    first_name: 'Joe',
+    last_name: 'Toe',
+    email: 'joe.toe@email.com'
+  },
+  {
+    first_name: 'Edgar',
+    last_name: 'Poe',
+    email: 'edgar.poe@email.com'
+  },
+])
