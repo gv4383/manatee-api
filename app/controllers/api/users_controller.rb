6f96b881ef1574cd967b8eea249d64a7e0ef2c23
gv4383 <gregory.vargas18@gmail.com>
2020-05-06 00:18:03 -0500

    Created users#show method

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -5,5 +5,11 @@ module Api
 
       render json: UserSerializer.new(users).serialized_json
     end
+
+    def show
+      user = User.find(params[:id])
+
+      render json: UserSerializer.new(user).serialized_json
+    end
   end
 end
