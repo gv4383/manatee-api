98e5156e60bb36f1412748e7ed7a099d7244b063
gv4383 <gregory.vargas18@gmail.com>
2020-06-29 00:19:09 -0500

    Add put to list of allowed methods for CORS

--- config/application.rb
+++ config/application.rb
@@ -32,7 +32,7 @@ module ManateeApi
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', headers: :any, methods: [:get, :post, :delete, :options]
+        resource '*', headers: :any, methods: [:get, :post, :put, :delete, :options]
       end
     end
 
